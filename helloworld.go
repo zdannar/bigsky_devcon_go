79a3807f0c3d8fca7520ba9fc6a774daa796e45f
zdannar <zdannar@gmail.com>
2016-06-07 21:53:04 -0600

    Update

--- helloworld.go
+++ helloworld.go
@@ -4,18 +4,19 @@ import (
 	"fmt"
 )
 
+func main() { // HLmain
+	a := &audience{"BigSky DevCon"} // HLmain
+	fmt.Printf("Hello %s!\n", a)    // HLmain
+} // HLmain
+
 // STRUCT OMIT
 type audience struct {
 	statement string
 }
 
+// Method of type audience
 func (a *audience) String() string {
 	return a.statement
 }
 
 // STRUCTE OMIT
-
-func main() { // HLmain
-	a := &audience{"BigSky DevCon"} // HLmain
-	fmt.Printf("Hello %s!\n", a)    // HLmain
-} // HLmain
